a7aed89ba9a0a831fb2d5ed4d3661c885868958c
AranethWinters <ernestblane.calara-19@cpu.edu.ph>
2024-12-05 02:16:23 +0800

    added google sign in option buttons

--- app/pages/signup/page.js
+++ app/pages/signup/page.js
@@ -1,15 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
+import { AiOutlineGoogle } from "react-icons/ai";
 
 const SignUp = () => {
   return (
     <div>
         <form>
             <label htmlFor='userEmail'>Enter Email</label>
-            <input type='email' id='SignUpEmail' className='text-black'></input>
+            <input type='email' id='SignUpEmail' className='text-black bg-pink-900'></input>
             <label htmlFor='userPass'>Enter Password</label>
-            <input type='text' id='SignUpPass' className='text-black'></input>
+            <input type='text' id='SignUpPass' className='text-black bg-pink-900'></input>
             <button><Link href='/pages/dashboard'>SignUp</Link></button>
+            <button><Link href='/pages/dashboard'>Already have an account?</Link></button>
+            <button><Link href=''><AiOutlineGoogle size={30}/></Link></button>
         </form>
     </div>
   )
