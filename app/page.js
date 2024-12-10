236131f390518f0c19988c1cf24be6135808bd0c
CJ Armada <cjs.armada@gmail.com>
2024-12-10 18:24:01 +0800

    added navbar

--- app/page.js
+++ app/page.js
@@ -2,19 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="navbar">
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Our Games</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Sign in</a></li>
-            <li><a href="#">Register</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+    <div></div>
   );
 }
