7a6c359fd44d6588d222b6ea4d97b7c24c33754c
CJ Armada <cjs.armada@gmail.com>
2024-12-10 18:50:55 +0800

    NavBarDesign

--- app/pages/home/page.js
+++ app/pages/home/page.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import Header from '@/app/components/header/page'
 
 const Homepage = () => {
   return (
     <div>
+      <Header/>
       <button><Link href="/pages/signin">Signin</Link></button>
       <button><Link href="/pages/signup">Signup</Link></button>
     </div>
