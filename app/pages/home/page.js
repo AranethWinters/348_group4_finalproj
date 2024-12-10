c947b53727d78255bf9ad5f522da50e8d2ec8240
Seiza03 <elijahsanagustin03@gmail.com>
2024-12-10 17:48:54 +0700

    added footer

--- app/pages/home/page.js
+++ app/pages/home/page.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
+import Footer from '@/app/components/footer/page'
 
 const Homepage = () => {
   return (
     <div>
       <button><Link href="/pages/signin">Signin</Link></button>
       <button><Link href="/pages/signup">Signup</Link></button>
+      <Footer/>
     </div>
   )
 }
