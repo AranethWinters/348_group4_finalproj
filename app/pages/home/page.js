256311cd2d110ce32f914a6e483beccee1d02eab
Seiza03 <elijahsanagustin03@gmail.com>
2024-12-15 16:59:13 +0700

    About us section

--- app/pages/home/page.js
+++ app/pages/home/page.js
@@ -3,15 +3,17 @@ import React from 'react'
 import Footer from '@/app/components/footer/page'
 import CardList from '@/app/components/placeholder_card/page'
 import Header from '@/app/components/header/page'
-
+import AboutUsSummary from '@/app/components/about_us/page'
 const Homepage = () => {
   return (
-    <div>
-      <CardList></CardList>
+    <div> 
       <Header/>
+      <AboutUsSummary></AboutUsSummary>
+      <CardList></CardList>
       <button><Link href="/pages/signin">Signin</Link></button>
       <button><Link href="/pages/signup">Signup</Link></button>
       <Footer/>
+    
     </div>
   )
 }
