e591c7d401ad64045fd695bba4902f0f824e89a9
Pete01723 <gamerstagged@yahoo.com>
2024-12-16 20:52:18 +0800

    Initial fixes to shipping and order

---
+++ app/pages/order/page.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+function Order() {
+  return (
+    <div className='flex justify-center gap-8'>
+        <div className='min-w-min max-w-fit border'>
+            <form>
+                <p>Payment</p>
+                <hr/>
+                <p>Pay With:</p>
+                <div>
+                    <label>
+                        <input type='radio' name='radioCard' value="card"/>
+                        Card
+                    </label>
+                    <label>
+                        <input type='radio' name='radioBank' value="bank"/>
+                        Bank
+                    </label>
+                    <label>
+                        <input type='radio' name='radioTransfer' value="transfer"/>
+                        Transfer
+                    </label>
+                </div>
+                <p>Card Number</p>
+                <input name='cardNo'/>
+                <div>
+                    <p>Expiration Date</p>
+                    <input name='expDate'/>
+                    <p>CVV</p>
+                    <input name='cvv'/>
+                </div>
+                <label>
+                    <input type='checkbox' name='saveCard'/>
+                    Save card details
+                </label>
+                <hr/>
+                <button type='submit'>Pay USD</button>
+                <p>
+                    Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our privacy policy.
+                </p>
+            </form>
+        </div>
+        <div className='border'>
+            <form>
+                <p>Order Summary</p>
+                <hr/>
+                <div>
+
+                </div>
+                <hr/>
+                <input name='giftCode' placeholder='Gift or discount code'/>
+                <button>Apply</button>
+                <hr/>
+                <div>
+                    <p>Subtotal</p>
+                    <p>$</p>
+                </div>
+                <div>
+                    <p>Shipping</p>
+                    <p>$</p>
+                </div>
+                <hr/>
+                <div>
+                    <p>Total</p>
+                    <p>$</p>
+                </div>      
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Order
--- app/pages/shipping/page.js
+++ app/pages/shipping/page.js
@@ -1,55 +1,45 @@
 import React from 'react'
-import Header from '@/app/components/header/page'
-import Footer from '@/app/components/footer/page'
 import Image from 'next/image'
 import Placeholder from '../../assets/images/image.png'
 
 const Shipping = () => {
   return (
-    <div>
-        <Header/>
-        <main>
-            <div className='flex justify-center my-8'>
-                <div className='grid grid-cols-5'>
-                    <div className='col-span-3'>
-                        <Image 
-                            src={Placeholder}
-                            width= {160} 
-                            height={160} 
-                            alt='placholder image' 
-                            className=''
-                        />
-                    </div>
-                    <div>
-                        <form>
-                            <p>Shipping Information</p>
-                            <p>We ship within 2 working days</p>
-                            <hr/>
-                            <p>Full Name</p>
-                            <input name='fullname' placeholder='Value'/>
-                            <hr/>
-                            <p>Location</p>
-                            <select name='location'>
-                                <option value='Iloilo'>Iloilo</option>
-                                <option value='Capiz'>Capiz</option>
-                                <option value='Aklan'>Aklan</option>
-                                <option value='Antique'>Antique</option>
-                            </select>
-                            <hr/>
-                            <p>Delivery Note</p>
-                            <textarea name='deliverynote'/>
-                            <hr/>
-                            <label>
-                                <input type='checkbox' name='tc'/> I accept the terms 
-                            </label>
-                            <hr/>
-                            <button type='submit'>Save shipping information</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </main>
-        <Footer/>
+    <div className='flex flex-row justify-center h-screen w-screen gap-8 p-8'>
+        <div className='basis-full h-1/2 content-center border'>
+            <Image 
+                src={Placeholder}
+                width= {160} 
+                height={160} 
+                alt='placholder image' 
+                className='mx-auto my-auto object-fill aspect-video'
+            />
+        </div>
+        <div className='basis-1/4 h-1/2 p-8 border'>
+            <form className='object-contain'>
+                <p>Shipping Information</p>
+                <p>We ship within 2 working days</p>
+                <hr/>
+                <p>Full Name</p>
+                <input name='fullname' placeholder='Value'/>
+                <hr/>
+                <p>Location</p>
+                <select name='location'>
+                    <option value='Iloilo'>Iloilo</option>
+                    <option value='Capiz'>Capiz</option>
+                    <option value='Aklan'>Aklan</option>
+                    <option value='Antique'>Antique</option>
+                </select>
+                <hr/>
+                <p>Delivery Note</p>
+                <textarea name='deliverynote'/>
+                <hr/>
+                <label>
+                    <input type='checkbox' name='tc'/> I accept the terms 
+                </label>
+                <hr/>
+                <button type='submit'>Save shipping information</button>
+            </form>
+        </div>
     </div>
   )
 }
