9aac8094d4b7d8d04ee902ae3f6afd72208b729a
AranethWinters <ernestblane.calara-19@cpu.edu.ph>
2024-12-17 14:41:41 +0800

    fixing deployment issues

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,13 +1,10 @@
 'use client'
 import Link from 'next/link'
 import React, { useState } from 'react'
-import { auth } from '../../lib/firebase/clientApp'
-import Authentication from './../Authentication.jsx' 
+import Authentication from './../Authentication.jsx';
 
 
 function Header(){
-  const [user, setUser] = useState(auth.currentUser);
-  console.log(user)
   return (
     <div className="container">
       <div className="navbar">
