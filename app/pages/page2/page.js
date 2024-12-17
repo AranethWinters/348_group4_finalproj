fc8d64c493279beef5450ae2ae34d77276c4ff9a
Ernest Blane Calara <156556665+AranethWinters@users.noreply.github.com>
2024-12-17 13:08:58 +0000

    fixed header misalignment

--- app/pages/page2/page.js
+++ app/pages/page2/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Game1 = () => {
   return (
     <div>
-      <main className="container">
+      <main className="container-gameinfo">
         <div className='game-details'>
           <h1 className="game-title">Game 1</h1>
           <button className="add-to-cart">Add to Cart</button>
